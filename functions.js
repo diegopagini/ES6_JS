e44a40a517231bd22d927cf412658d99104508e9
Diego Pagini <diegopaginidev@gmail.com>
2022-11-01 10:18:03 +0100

    REST

--- functions.js
+++ functions.js
@@ -33,3 +33,45 @@ function fnTemporal() {
 }
 
 greeting3(fnTemporal, { name: 'Test' });
+
+// sin argumentos opcionales.
+function sum(a, b) {
+	console.log(arguments);
+}
+sum(1, 2, 3, { name: 'test' }); // [1, 2, 3, {name: 'Test'}]
+
+// con argumentos opcionales.
+function sum2(a = 1, b = 2) {
+	console.log(arguments);
+}
+sum2(); // []
+
+// Parámetro REST
+
+// ES5
+function addStudents() {
+	console.log(arguments);
+	for (let i = 1; i < arguments.length; i++) {
+		arguments[0].push(arguments[i]);
+	}
+
+	return arguments[0];
+}
+
+addStudents();
+
+const studentsArr = ['Diego'];
+const sutdentsArr2 = addStudents(studentsArr, 'Maria', 'Pepe', 'Susana', 'Juan');
+console.log(sutdentsArr2);
+
+// ES6
+function addStudents2(sutdentsArr, ...students) {
+	for (let i = 0; i < students.length; i++) {
+		sutdentsArr.push(students[i]);
+	}
+
+	return sutdentsArr;
+}
+
+const studentsArr3 = addStudents2(studentsArr, 'Pedro', 'Pablo', 'Elizabet');
+console.log(studentsArr3);
