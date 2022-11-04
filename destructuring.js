c3ee7a9dfbb590685b5e8f65d6831da0a8cc3763
Diego Pagini <diego.pagini@neoris.com>
2022-11-04 15:01:06 +0100

    destructuring

--- destructuring.js
+++ destructuring.js
@@ -18,3 +18,46 @@ console.log(name2, email2); // Diego diegopaginidev@gmail.com.test
 // ES6
 const { name, email, premium: dePago, twitter = 'test@test' } = ajustes; // premium: dePago es como utilizar el "as"
 console.log(name, email, dePago, twitter); // Diego diegopaginidev@gmail.com.test true test@test
+
+let autoGuardado = {
+	archivo: 'app.js',
+	cursor: {
+		linea: 7,
+		columna: 16,
+	},
+	ultimoArchivo: {
+		archivo: 'index.html',
+		cursor: {
+			linea: 8,
+			columna: 20,
+		},
+	},
+	otroNodo: {
+		subNodo: {
+			cursor: {
+				linea: 11,
+				columna: 2,
+			},
+		},
+	},
+};
+
+let { cursor: cursor2 } = autoGuardado;
+console.log(cursor2); // { linea: 7, columna: 16 }
+
+// Objetos anidados:
+let {
+	ultimoArchivo: { cursor: ultimoArchivo },
+} = autoGuardado;
+console.log(ultimoArchivo); // { linea: 8, columna: 20 }
+
+// Objetos anidados de anidados:
+let {
+	otroNodo: {
+		subNodo: { cursor: otroSuperNodo },
+	},
+} = autoGuardado;
+
+console.log(otroSuperNodo); // { linea: 11, columna: 2 }
+let otroSuperNodo2 = autoGuardado.otroNodo.subNodo.cursor;
+console.log(otroSuperNodo2); // { linea: 11, columna: 2 }
