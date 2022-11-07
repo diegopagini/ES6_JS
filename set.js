02859151f60ba26132758df7b1c5906afd390acc
Diego Pagini <diego.pagini@neoris.com>
2022-11-07 10:07:48 +0100

    sets

--- set.js
+++ set.js
@@ -27,3 +27,40 @@ console.log(items2.size); // 4
 
 // has() para saber si existe
 console.log(items.has(3)); // true
+
+let items3 = new Set([1, 2, 3, 4, 5]);
+console.log(items3.size); // 5
+
+// delete
+items3.delete(3); // delete("aqui va el elemento")
+console.log(items3); // Set(4) {1, 2, 4, 5}
+console.log(items3.size); // 4
+
+// clear
+items3.clear();
+console.log(items3); // Set(0)
+console.log(items3.size); // 0
+
+// forEach
+let personas = new Set(['Diego', 'Juan', 'Pedro']);
+
+personas.forEach((valor, llave, setOriginal) => {
+	console.log(valor, llave, setOriginal); // Set(3) {'Diego', 'Juan', 'Pedro'}
+	console.log(personas === setOriginal); // true
+});
+
+// Set to Array
+let numeros = [1, 2, 3, 3, 3, 3, 3, 4, 5, 6, 7, 7, 7, 7, 7, 7, 7, 8, 9, 10];
+
+let setNumeros = new Set(numeros);
+
+console.log(setNumeros);
+
+let arrayNumeros = [...setNumeros];
+
+console.log(arrayNumeros); //  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// La forma mas fácil para eliminar los duplicados de un array.
+function deleteDuplicateOnes(items) {
+	return [...new Set(items)];
+}
