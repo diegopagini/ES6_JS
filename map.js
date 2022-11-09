71b958feea44892f1c27f01882019552a79f233b
Diego Pagini <diegopaginidev@gmail.com>
2022-11-09 13:38:26 +0100

    maps

--- map.js
+++ map.js
@@ -1,1 +1,23 @@
 /** @format */
+
+// Maps - methods.
+
+let map = new Map();
+
+map.set('name', 'Diego');
+map.set('age', 30);
+
+console.log(map); // Map {"name" => "Diego", "age" => 30}
+console.log(map.size); // 2
+console.log(map.get('name')); // Diego
+console.log(map.get('age')); // 30
+console.log(map.has('name')); // true
+
+map.delete('name');
+console.log(map.has('name')); // false
+
+map.set('age');
+console.log(map.get('age')); // undefined
+
+map.clear();
+console.log(map); // {}
